23e2c8c66114fbce446c2ac06be7f9941f4eefd5
aleksandr-kalmychkov <aleksandr.kalmychkov@gmail.com>
2022-09-29 08:58:38 +0500

    Realised function SumOfPositive that takes an vector and return summ of all positive elements.

--- GHLesson/GHLesson.cpp
+++ GHLesson/GHLesson.cpp
@@ -1,5 +1,17 @@
-﻿#include <iostream>
+﻿#include <vector>
+#include <iostream>
+using namespace std;
 
 int main()
 {
+}
+
+int SumOfPositive(vector<int> arr)
+{
+	int summ = 0;
+	for (auto obj : arr)
+	{
+		summ += obj > 0 ? obj : 0;
+	}
+	return summ;
 }
